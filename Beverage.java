d2355504170024481cc1b8f5047da99f2b8fa8b0
ToyBitZ00 <pa6308191@gmail.com>
2025-09-06 14:34:47 +0800

    Added print statements to display beverage options, and let the user choose one, also added if else statements to display what beverage the user chose.

--- Beverage.java
+++ Beverage.java
@@ -4,6 +4,31 @@ public class Beverage {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
 
+        int beverage;
+
+        System.out.println("[1] Coke");
+        System.out.println("[2] Sprite");
+        System.out.println("[3] Royal");
+        System.out.println("[4] Rootbeer");
+        System.out.println("[5] Pepsi");
+
+        System.out.print("Enter code (1-5): ");
+        beverage = object.nextInt();
+
+        // Selecting a beverage using If else statement
+        if (beverage == 1) {
+            System.out.println("Beverage is Coke");
+        } else if (beverage == 2) {
+            System.out.println("Beverage is Sprite");
+        } else if (beverage == 3) {
+            System.out.println("Beverage is Royal");
+        } else if (beverage == 4) {
+            System.out.println("Beverage is Rootbeer");
+        } else if (beverage == 5) {
+            System.out.println("Beverage is Pepsi");
+        } else {
+            System.out.println("Invalid Input");
+        }
         object.close();
     }
 }
